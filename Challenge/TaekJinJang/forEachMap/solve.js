9e2620091d75c5eed2a6c87d1272c8eb19bbfd50
TaekJin Jang <xorwls1500@naver.com>
2022-02-13 22:08:32 +0900

    -

---
+++ Challenge/TaekJinJang/forEachMap/solve.js
@@ -0,0 +1,7 @@
+// write your codes
+let inputArray = [100, 10, 20, 40];
+let output = [];
+inputArray.forEach((element) => {
+  output.push(element + "%");
+});
+console.log(output);
---
+++ Challenge/TaekJinJang/forEachReduce/solve.js
@@ -0,0 +1,9 @@
+// write your codes
+let inputArray = [-10, -20, -30];
+
+let sum = 0;
+
+inputArray.forEach((item) => {
+  sum += item;
+});
+console.log(sum);
